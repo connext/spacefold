75782f5fe82706223f1bef1fb1abab08384d86c4
LayneHaber <layne.haber@gmail.com>
2020-07-24 20:00:43 -0700

    calling faucet and transfer successfully

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import * as connext from "@connext/client";
-import { ConditionalTransferTypes } from "@connext/types";
-import { constants } from "ethers";
+import { constants, BigNumber } from "ethers";
 import Select from "react-select";
 
 import "./App.css";
 import { getWallet } from "./wallet";
+import { parseEther, formatEther } from "ethers/lib/utils";
 const dotenv = require("dotenv");
 dotenv.config();
 
 const nodeUrl = "https://node.spacefold.io/";
+// const nodeUrl = "http://localhost:8080";
 
 const networks = {
-  1: { name: "Mainnet", chainId: 1 },
+  // 1: { name: "Mainnet", chainId: 1 },
   4: { name: "Rinkeby", chainId: 4 },
-  5: { name: "Goerli", chainId: 5 },
+  // 5: { name: "Goerli", chainId: 5 },
   42: { name: "Kovan", chainId: 42 },
+  // 1337: { name: "Ganache", chainId: 1337 },
+  // 1338: { name: "Buidler", chainId: 1338 },
 };
 
 const tokens = {
   eth: { tokenName: "ETH", tokenAddress: constants.AddressZero },
-  moon: { tokenName: "MOON", tokenAddress: constants.AddressZero },
-  brick: { tokenName: "BRICK", tokenAddress: constants.AddressZero },
+  moon: {
+    tokenName: "MOON",
+    tokenAddress: "0x50C94BeCAd95bEe21aF226dc799365Ee6B134459", // rinkeby
+  },
+  brick: {
+    tokenName: "BRICK",
+    tokenAddress: "0x4d4deb65DBC13dE6811095baba7064B41A72D9Db", // kovan
+  },
 };
 
 function App() {
@@ -43,7 +53,6 @@ function App() {
 
   useEffect(() => {
     async function initClients() {
-      console.log("Trying to init clients in env", process.env);
       const clientsArr = [];
       for (const network of Object.values(networks)) {
         try {
@@ -68,19 +77,36 @@ function App() {
       });
       setClients(_clients);
       setTweets(_tweets);
+      console.error("set _clients", _clients);
     }
     initClients();
   }, []);
 
   useEffect(() => {
-    // TODO:  get balances from respective clients
-    const mintTokens = [{ ...networks[1], ...tokens.eth, balance: 0.1 }];
-    const sendTokens = [
-      { ...networks[4], ...tokens.moon, balance: 0 },
-      { ...networks[5], ...tokens.brick, balance: 0 },
-    ];
-    setMintTokens(mintTokens);
-    setSendTokens(sendTokens);
+    const updateTokenBalance = async () => {
+      const mintClient = clients[networks[4].chainId];
+      const mintBal = !!mintClient
+        ? (await mintClient.getFreeBalance(tokens.moon.tokenAddress))[
+            mintClient.signerAddress
+          ]
+        : BigNumber.from(0);
+      const mintTokens = [
+        { ...networks[4], ...tokens.moon, balance: formatEther(mintBal) },
+      ];
+
+      const sendClient = clients[networks[42].chainId];
+      const sendBal = !!sendClient
+        ? (await sendClient.getFreeBalance(tokens.brick.tokenAddress))[
+            sendClient.signerAddress
+          ]
+        : BigNumber.from(0);
+      const sendTokens = [
+        { ...networks[42], ...tokens.brick, balance: formatEther(sendBal) },
+      ];
+      setMintTokens(mintTokens);
+      setSendTokens(sendTokens);
+    };
+    updateTokenBalance();
   }, [clients]);
 
   const changeMintToken = (option) => {
@@ -102,28 +128,60 @@ function App() {
 
     const toToken = sendTokens[activeSendToken];
     const toClient = clients[toToken.chainId];
+    console.error(
+      "recipient pre transfer balance",
+      (await toClient.getFreeBalance(toToken.tokenAddress))[
+        toClient.signerAddress
+      ].toString()
+    );
 
-    await fromClient.conditionalTransfer({
-      conditionType: ConditionalTransferTypes.LinkedTransfer,
+    const params = {
       assetId: fromToken.tokenAddress,
-      amount: fromToken.balance,
+      amount: parseEther(fromToken.balance.toString()),
       recipient: toClient.publicIdentifier,
       meta: {
         receiverAssetId: toToken.tokenAddress,
         receiverChainId: toToken.chainId,
       },
+    };
+    console.warn("*** calling transfer with params", params);
+    await fromClient.transfer(params);
+    toClient.once("CONDITIONAL_TRANSFER_UNLOCKED_EVENT", async (data) => {
+      console.error("got conditional transfer unlocked event", data);
+      console.error(
+        "recipient post transfer balance",
+        (await toClient.getFreeBalance(toToken.tokenAddress))[
+          toClient.signerAddress
+        ].toString()
+      );
     });
   };
 
   const mint = async () => {
     const mintToken = mintTokens[activeMintToken];
     const assetId = mintToken.tokenAddress;
-    const recipient = clients[mintToken.chainId].publicIdentifier;
-    const tweet = tweets[mintToken.chainId].publicIdentifier;
-    await fetch({
-      method: "POST",
-      url: `${process.env.REACT_APP_FAUCET_URL}/faucet`,
-      body: JSON.stringify({ assetId, recipient, tweet }),
+    const client = clients[mintToken.chainId];
+    if (!client) {
+      console.error(`Failed to find client for ${mintToken.chainId}`, clients);
+      return;
+    }
+    console.error(
+      "pre faucet balance",
+      (await client.getFreeBalance(assetId))[client.signerAddress].toString()
+    );
+    console.log("faucet url", `${process.env.REACT_APP_FAUCET_URL}/faucet`);
+    const res = await axios.post(`${process.env.REACT_APP_FAUCET_URL}/faucet`, {
+      assetId,
+      recipient: client.publicIdentifier,
+      tweet: "devmode",
+    });
+    console.log("res", res);
+    client.once("CONDITIONAL_TRANSFER_UNLOCKED_EVENT", async (data) => {
+      console.error("got conditional transfer unlocked event", data);
+      console.error(
+        "post faucet balance",
+        (await client.getFreeBalance(assetId))[client.signerAddress].toString()
+      );
     });
   };
 
