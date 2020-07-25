72c50f114c009153952279850419fa3a599cc4b6
LayneHaber <layne.haber@gmail.com>
2020-07-24 22:47:12 -0700

    one mnemonic per client

--- src/wallet.js
+++ src/wallet.js
@@ -1,21 +1,21 @@
 import { Wallet } from "ethers";
 
 export const ETH_STANDARD_PATH = "m/44'/60'/0'/0";
-export const MNEMONIC_KEY = "MNEMONIC_PHRASE";
+export const MNEMONIC_KEY = "MNEMONICS";
 
 export function getPath(index) {
   return `${ETH_STANDARD_PATH}/${index}`;
 }
 
-export function getMnemonic() {
-  let mnemonic = localStorage.getItem(MNEMONIC_KEY);
-  if (!mnemonic) {
-    mnemonic = Wallet.createRandom().mnemonic.phrase;
-    localStorage.setItem(MNEMONIC_KEY, mnemonic);
+export function getMnemonic(chainId) {
+  const mnemonics = JSON.parse(localStorage.getItem(MNEMONIC_KEY) || "{}");
+  if (!mnemonics[chainId]) {
+    mnemonics[chainId] = Wallet.createRandom().mnemonic.phrase;
+    localStorage.setItem(MNEMONIC_KEY, JSON.stringify(mnemonics));
   }
-  return mnemonic;
+  return mnemonics[chainId];
 }
 
-export function getWallet(index) {
-  return Wallet.fromMnemonic(getMnemonic(), getPath(index));
+export function getWallet(chainId) {
+  return Wallet.fromMnemonic(getMnemonic(chainId.toString()));
 }
