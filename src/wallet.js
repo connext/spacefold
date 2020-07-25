5733b968cd0b352c6d11f89c5f988221ba3cffad
LayneHaber <layne.haber@gmail.com>
2020-07-24 20:14:36 -0700

    prettier

--- src/wallet.js
+++ src/wallet.js
@@ -1,25 +1,21 @@
-import { Wallet } from "ethers"
-
+import { Wallet } from "ethers";
 
 export const ETH_STANDARD_PATH = "m/44'/60'/0'/0";
-export const MNEMONIC_KEY = "MNEMONIC_PHRASE"
-
+export const MNEMONIC_KEY = "MNEMONIC_PHRASE";
 
 export function getPath(index) {
   return `${ETH_STANDARD_PATH}/${index}`;
 }
 
 export function getMnemonic() {
-  let mnemonic = localStorage.getItem(MNEMONIC_KEY)
+  let mnemonic = localStorage.getItem(MNEMONIC_KEY);
   if (!mnemonic) {
-    mnemonic = Wallet.createRandom().mnemonic.phrase
-    localStorage.setItem(MNEMONIC_KEY, mnemonic)
+    mnemonic = Wallet.createRandom().mnemonic.phrase;
+    localStorage.setItem(MNEMONIC_KEY, mnemonic);
   }
-  return mnemonic
+  return mnemonic;
 }
 
 export function getWallet(index) {
-  return Wallet.fromMnemonic(getMnemonic(), getPath(index))
+  return Wallet.fromMnemonic(getMnemonic(), getPath(index));
 }
-
-
