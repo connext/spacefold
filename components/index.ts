a3c55557d21f50b61361f67ceed16f5384a02100
sanchaymittal <sanchaymittal@gmail.com>
2020-12-18 14:07:58 +0530

    export new components

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,7 @@
 import Loading from "./Loading";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Card from "./Card";
+import CrossChainCard from "./CrossChainCard";
 
-export { Loading, Navbar, Footer };
+export { Loading, Navbar, Footer, Card, CrossChainCard };
