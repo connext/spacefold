91e3db8ff8db621c18a8d2992ec132a513bf10db
sanchaymittal <sanchaymittal@gmail.com>
2021-01-05 02:32:12 +0530

    update spacefold

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,13 +1,159 @@
 import React, { useState } from "react";
 import { ConnextModal } from "@connext/vector-modal";
-import { Button, Divider, Form, Input } from "antd";
+import {
+  Grid,
+  Button,
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+import { utils } from "ethers";
+import { Divider, Form, Input } from "antd";
 
 export default function Modal() {
   const [showModal, setShowModal] = useState(false);
-  const [form] = Form.useForm();
+
+  const [withdrawalAddress, setWithdrawalAddress] = useState("");
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = (event) => {
+    setWithdrawalAddress(event.target.value);
+  };
+
+  const handleSubmit = (values) => {
+    const errors = { receiverAddress: "" };
+    if (!values.receiverAddress) {
+      errors.receiverAddress = "Required";
+    }
+    return errors;
+  };
+
+  const CHAIN_INFO_URL = "https://chainid.network/chains.json";
+  const getNetworkName = async (chainId): Promise<string> => {
+    let chainName: string;
+    try {
+      const chainInfo: any[] = await utils.fetchJson(CHAIN_INFO_URL);
+
+      const getChainInfo = chainInfo.find((info) => info.chainId === chainId);
+      if (getChainInfo) {
+        chainName = getChainInfo.name;
+      }
+    } catch (e) {
+      console.warn(`Could not fetch chain info from ${CHAIN_INFO_URL}`);
+    }
+    return chainName;
+  };
+  interface NETWORK {
+    depositChainId: number;
+    depositChainName: string;
+    withdrawChainId: number;
+    withdrawChainName: string;
+    tokens: TOKEN[];
+  }
+  interface TOKEN {
+    name: string;
+    depositAssetId: string;
+    withdrawAssetId: string;
+  }
+
+  const GOERLI_MATIC_TOKENS: TOKEN[] = [
+    {
+      name: "TestToken",
+      depositAssetId: "0xbd69fC70FA1c3AED524Bb4E82Adc5fcCFFcD79Fa",
+      withdrawAssetId: "0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1",
+    },
+  ];
+
+  const RINKEBY_KOVAN_TOKENS: TOKEN[] = [
+    {
+      name: "ETH",
+      depositAssetId: "0x0000000000000000000000000000000000000000",
+      withdrawAssetId: "0x0000000000000000000000000000000000000000",
+    },
+  ];
+  const networks: NETWORK[] = [
+    {
+      depositChainId: 5,
+      depositChainName: "Goerli Testnet",
+      withdrawChainId: 80001,
+      withdrawChainName: "Matic Testnet",
+      tokens: GOERLI_MATIC_TOKENS,
+    },
+    {
+      depositChainId: 4,
+      depositChainName: "Rinkeby Testnet",
+      withdrawChainId: 42,
+      withdrawChainName: "Kovan Testnet",
+      tokens: RINKEBY_KOVAN_TOKENS,
+    },
+  ];
+
+  const handleNetwork = (event) => {
+    setChain(networks[event.target.value]);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const [chain, setChain] = useState<NETWORK>(networks[0]);
   return (
     <>
-      <Form form={form} onFinish={() => setShowModal(true)}>
+      <form onSubmit={handleSubmit} noValidate>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Select
+              id="demo-controlled-open-select"
+              open={open}
+              onClose={handleClose}
+              onOpen={handleOpen}
+              onChange={handleNetwork}
+              fullWidth
+              defaultValue={0}
+              // component={Select}
+            >
+              {networks.map((t, index) => {
+                return (
+                  <MenuItem value={index}>
+                    {t.depositChainName} to {t.withdrawChainName}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Receiver Address"
+              name="receiverAddress"
+              aria-describedby="receiverAddress"
+              defaultValue={withdrawalAddress}
+              type="search"
+              onChange={handleChange}
+              required
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+      </form>
+      <Grid container spacing={2}>
+        <Grid item style={{ marginTop: 16 }}>
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            disabled={!withdrawalAddress || !chain}
+            onClick={() => setShowModal(true)}
+          >
+            Cross-Chain Transfer
+          </Button>
+        </Grid>
+      </Grid>
+
+      {/* <Form form={form} onFinish={() => setShowModal(true)}>
         <Form.Item
           name="withdrawalAddress"
           rules={[
@@ -22,15 +168,16 @@ export default function Modal() {
             Goerli to Matic Testnet
           </button>
         </Form.Item>
-      </Form>
+      </Form> */}
+
       <ConnextModal
         showModal={showModal}
         routerPublicIdentifier="vector7tbbTxQp8ppEQUgPsbGiTrVdapLdU5dH7zTbVuXRf1M4CEBU9Q"
-        depositAssetId={"0xbd69fC70FA1c3AED524Bb4E82Adc5fcCFFcD79Fa"}
-        depositChainId={5}
-        withdrawAssetId={"0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1"}
-        withdrawChainId={80001}
-        withdrawalAddress={form.getFieldValue("withdrawalAddress")}
+        depositAssetId={chain!.tokens[0].depositAssetId}
+        depositChainId={chain!.depositChainId}
+        withdrawAssetId={chain!.tokens[0].withdrawAssetId}
+        withdrawChainId={chain!.withdrawChainId}
+        withdrawalAddress={withdrawalAddress}
         onClose={() => setShowModal(false)}
       />
     </>
