cae96655338a857be6bcb9949d3084878fb4b3be
Jake <jcb.kdd@gmail.com>
2021-02-26 19:23:34 -0800

    remove chakra provider, re-add tailwind to globals

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,6 @@ import { Loading, Navbar, Footer } from "../components";
 import MainCard from "./MainCard";
 import React, { useState, useEffect } from "react";
 
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from "@chakra-ui/react"
-
 export default function Home() {
   const [initializing, setInitializing] = useState(true);
   const loadingMessage = "Welcome";
@@ -16,16 +13,14 @@ export default function Home() {
 
   return (
     <div className="App">
-      <ChakraProvider>
-        <Head>
-          <title>Spacefold</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-        <Loading initializing={initializing} message={loadingMessage} />
-        <Navbar />
-        <MainCard/>
-        <Footer />
-      </ChakraProvider>
+      <Head>
+        <title>Spacefold</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Loading initializing={initializing} message={loadingMessage} />
+      <Navbar />
+      <MainCard/>
+      <Footer />
     </div>
   );
 }
