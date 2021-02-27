34d9043f68df09a6285d9020e9ccfd08b3782507
Jake <jcb.kdd@gmail.com>
2021-02-26 18:09:38 -0800

    wrap app with chakra provider (test)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import { Loading, Navbar, Footer } from "../components";
 import MainCard from "./MainCard";
 import React, { useState, useEffect } from "react";
 
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react"
+
 export default function Home() {
   const [initializing, setInitializing] = useState(true);
   const loadingMessage = "Welcome";
@@ -13,14 +16,16 @@ export default function Home() {
 
   return (
     <div className="App">
-      <Head>
-        <title>Spacefold</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Loading initializing={initializing} message={loadingMessage} />
-      <Navbar />
-      <MainCard/>
-      <Footer />
+      <ChakraProvider>
+        <Head>
+          <title>Spacefold</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Loading initializing={initializing} message={loadingMessage} />
+        <Navbar />
+        <MainCard/>
+        <Footer />
+      </ChakraProvider>
     </div>
   );
 }
