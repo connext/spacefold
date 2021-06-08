864237dad56ffb5140d606bc0aa1d5d5f5d2fe7f
LayneHaber <layne.haber@gmail.com>
2021-06-08 11:35:21 -0600

    remove stale arbitrum testnet

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -68,21 +68,21 @@ export default function Modal() {
     },
   ];
 
-  const KOVAN_ARBITRUM_TOKENS: TOKEN[] = [
-    {
-      name: "ETH",
-      depositAssetId: "0x0000000000000000000000000000000000000000",
-      withdrawAssetId: "0x0000000000000000000000000000000000000000",
-    },
-  ];
+  // const KOVAN_ARBITRUM_TOKENS: TOKEN[] = [
+  //   {
+  //     name: "ETH",
+  //     depositAssetId: "0x0000000000000000000000000000000000000000",
+  //     withdrawAssetId: "0x0000000000000000000000000000000000000000",
+  //   },
+  // ];
 
-  const ARBITRUM_KOVAN_TOKENS: TOKEN[] = [
-    {
-      name: "ETH",
-      depositAssetId: "0x0000000000000000000000000000000000000000",
-      withdrawAssetId: "0x0000000000000000000000000000000000000000",
-    },
-  ];
+  // const ARBITRUM_KOVAN_TOKENS: TOKEN[] = [
+  //   {
+  //     name: "ETH",
+  //     depositAssetId: "0x0000000000000000000000000000000000000000",
+  //     withdrawAssetId: "0x0000000000000000000000000000000000000000",
+  //   },
+  // ];
 
   // const ETH_MATIC_TOKENS: TOKEN[] = [
   //   {
@@ -129,20 +129,20 @@ export default function Modal() {
       withdrawChainName: "Rinkeby Testnet",
       tokens: KOVAN_RINKEBY_TOKENS,
     },
-    {
-      depositChainId: 42,
-      depositChainName: "Kovan Testnet",
-      withdrawChainId: 79377087078960,
-      withdrawChainName: "Arbitrum Testnet V3",
-      tokens: KOVAN_ARBITRUM_TOKENS,
-    },
-    {
-      depositChainId: 79377087078960,
-      depositChainName: "Arbitrum Testnet V3",
-      withdrawChainId: 42,
-      withdrawChainName: "Kovan Testnet",
-      tokens: ARBITRUM_KOVAN_TOKENS,
-    },
+    // {
+    //   depositChainId: 42,
+    //   depositChainName: "Kovan Testnet",
+    //   withdrawChainId: 79377087078960,
+    //   withdrawChainName: "Arbitrum Testnet V3",
+    //   tokens: KOVAN_ARBITRUM_TOKENS,
+    // },
+    // {
+    //   depositChainId: 79377087078960,
+    //   depositChainName: "Arbitrum Testnet V3",
+    //   withdrawChainId: 42,
+    //   withdrawChainName: "Kovan Testnet",
+    //   tokens: ARBITRUM_KOVAN_TOKENS,
+    // },
     // {
     //   depositChainId: 137,
     //   depositChainName: "Matic Mainnet",
